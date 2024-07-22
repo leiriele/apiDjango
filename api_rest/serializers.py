1cac5a1f0ede4c4e14907405dff8f464a48ac4a3
leiriele <leiri_leiri@hotmail.com>
2024-07-22 12:09:06 -0300

    Configurando o serializers

---
+++ api_rest/serializers.py
@@ -0,0 +1,14 @@
+from typing import __all__
+from rest_framework import serializers
+from .models import User, UserTasks
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: User
+        fields: '__all__' # os dados que deseja retornar 
+"""
+class UserTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: UserTasks
+        fields: 'user_nickname' 'user_task'
+""" 
