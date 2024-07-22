86f696b92ae42e393cfc71a2db363d44a2afe966
leiriele <leiri_leiri@hotmail.com>
2024-07-22 13:23:36 -0300

    Exibindo display para cadastro de novos usuarios

--- api_rest/admin.py
+++ api_rest/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import User
+# Lembre de criar um superUser:  python3 manage.py createsuperuser
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_nickname', 'user_name', 'user_email', 'user_age')
+    search_fields = ('user_nickname', 'user_name', 'user_email')
