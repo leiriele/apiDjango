2ba58778a7888ba63697fdddce33336175bd0adf
leiriele <leiri_leiri@hotmail.com>
2024-07-22 12:50:27 -0300

    Url e metodo GET

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
+import json
 
-# Create your views here.
+from . import funcoes as fn
+
+#Metodo GET
+@api_view(['GET'])
+def get_users(request):
+    if request.method == 'GET':
+        users = User.objects.all()                         
+        serializer = UserSerializer(users, many=True)      
+        return Response(serializer.data)                   
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+#metodo POST
+#@api_view(['POST'])
+#metodo PUT
+#@api_view(['PUT'])
+#metodo DELETE
+#@api_view(['DELETE'])
+
